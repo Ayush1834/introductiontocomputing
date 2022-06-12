ae4722052f6963bca09f7579d364a41d2eae320d
Ayush1834 <103477994+Ayush1834@users.noreply.github.com>
2022-06-12 23:00:46 +0530

    Add files via upload

---
+++ Assignment 4 Q3 output.py
@@ -0,0 +1,24 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: C:/Users/DELL/Desktop/Assignment 4 Question 3.py ==========
+Question 1: 8*7 = 54
+Wrong. The answer is  56
+Question 2: 8*1 = 8
+Right!
+Question 3: 9*5 = 45
+Right!
+Question 4: 5*1 = 7
+Wrong. The answer is  5
+Question 5: 3*7 = 34
+Wrong. The answer is  21
+Question 6: 2*9 = 18
+Right!
+Question 7: 3*2 = 6
+Right!
+Question 8: 4*3 = 13
+Wrong. The answer is  12
+Question 9: 2*7 = 15
+Wrong. The answer is  14
+Question 10: 8*5 = 40
+Right!
---
+++ Assignment 4 Question 4.py
@@ -0,0 +1,5 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======== RESTART: C:\Users\DELL\Desktop\PYTHON ASSN 4\Assignment 4 Q4.py =======
+Number of candies in the box are 177
